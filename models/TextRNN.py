53de2b58c0d1ad3d4074b769cbb5da41bc8f0e2a
limengyang1992 <73928007+limengyang1992@users.noreply.github.com>
2021-01-22 14:08:21 +0800

    Update TextRNN.py

--- models/TextRNN.py
+++ models/TextRNN.py
@@ -3,6 +3,23 @@ import torch
 import torch.nn as nn
 
 
+class Attention(torch.nn.Module):
+
+    def __init__(self, hidden_dim):
+        super(Attention, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.tanh = nn.Tanh()
+        self.w = nn.Parameter(torch.zeros(hidden_dim))
+
+    def forward(self, x):
+        H0 = self.tanh(x)
+        H1 = torch.matmul(H0, self.w)
+        H2 = nn.functional.softmax(H1, dim=1)
+        alpha = H2.unsqueeze(-1)
+        att_hidden = torch.sum(x * alpha, 1)
+        return att_hidden, H2
+
+
 class Model(nn.Module):
     def __init__(self, config):
         super(Model, self).__init__()
@@ -17,13 +34,16 @@ class Model(nn.Module):
                             1,    # num_layers
                             bidirectional=False,
                             batch_first=True)
+
+        self.attention = Attention(config.rnn_hidden_size)
+        
         self.fc = nn.Linear(config.rnn_hidden_size, len(config.class_idx))
 
     def forward(self, x):
         x = x.permute(1, 0)
         emb = self.embedding(x)
         emb = self.dropout(emb)
-        hidden, _ = self.lstm(emb)  # 句子最后时刻的 hidden state
-        hidden = hidden[:, -1, :]
-        out = self.fc(hidden)
+        hidden, _ = self.lstm(emb)  
+        attn, alpha = self.attention(hidden)
+        out = self.fc(attn)
         return out, hidden
